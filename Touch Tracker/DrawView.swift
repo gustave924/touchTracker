5c104e7dee4a14c4a303d40266fb32b925ab064b
Ahmed <madstorm20@gmail.com>
2019-07-12 00:57:27 +0200

    finished

--- Touch Tracker/DrawView.swift
+++ Touch Tracker/DrawView.swift
@@ -8,9 +8,18 @@
 
 import UIKit
 
-class DrawView: UIView{
+class DrawView: UIView, UIGestureRecognizerDelegate{
+    var moveGesture: UIPanGestureRecognizer!
     var currentLine = [NSValue:Line]()
     var finishedLines = [Line]()
+    var selectedIndex: Int?{
+        didSet{
+            if selectedIndex == nil{
+                let menu = UIMenuController.shared
+                menu.setMenuVisible(false, animated: true)
+            }
+        }
+    }
     
     @IBInspectable var finishedLineColor: UIColor = UIColor.black{
         didSet{
@@ -28,6 +37,109 @@ class DrawView: UIView{
         didSet{
             setNeedsDisplay()
         }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        let doubleTapRecognizer = UITapGestureRecognizer(target: self,action: #selector(DrawView.doubleTap(_:)))
+        doubleTapRecognizer.numberOfTapsRequired = 2
+        doubleTapRecognizer.delaysTouchesBegan = true
+        addGestureRecognizer(doubleTapRecognizer)
+        
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(DrawView.tap(_:)))
+        tapRecognizer.numberOfTapsRequired = 1
+        tapRecognizer.delaysTouchesBegan = true
+        tapRecognizer.require(toFail: doubleTapRecognizer)
+        addGestureRecognizer(tapRecognizer)
+        
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(DrawView.longPress(_:)))
+        addGestureRecognizer(longPressRecognizer)
+        
+        moveGesture = UIPanGestureRecognizer(target: self, action: #selector(DrawView.moveLine(_:)))
+        moveGesture.delegate = self
+        moveGesture.cancelsTouchesInView = false
+        addGestureRecognizer(moveGesture)
+    }
+    
+    @objc func doubleTap(_ gestureRecognizer: UIGestureRecognizer){
+        currentLine.removeAll()
+        finishedLines.removeAll()
+        selectedIndex = nil
+        setNeedsDisplay()
+    }
+    
+    @objc func deleteLine(_ sender: UIMenuController){
+        if let index = selectedIndex{
+            finishedLines.remove(at: index)
+            selectedIndex = nil
+            setNeedsDisplay()
+        }
+    }
+    
+    @objc func longPress(_ sender: UIGestureRecognizer){
+        setNeedsDisplay()
+        if(sender.state == .began){
+            let point = sender.location(in: self)
+            selectedIndex = indexOfLine(at: point)
+            
+            if selectedIndex != nil {
+                currentLine.removeAll()
+            }
+            
+        }else if(sender.state == .ended){
+            selectedIndex = nil
+        }
+        setNeedsDisplay()
+    }
+    
+    @objc func moveLine(_ sender: UIPanGestureRecognizer){
+        if let index = selectedIndex {
+            // When the pan recognizer changes its position...
+            if moveGesture.state == .changed {
+                // How far has the pan moved?
+                let translation = moveGesture.translation(in: self)
+                // Add the translation to the current beginning and end points of the line
+                // Make sure there are no copy and paste typos!
+                finishedLines[index].begin.x += translation.x
+                finishedLines[index].begin.y += translation.y
+                finishedLines[index].end.x += translation.x
+                finishedLines[index].end.y += translation.y
+                moveGesture.setTranslation(CGPoint.zero, in: self)
+                // Redraw the screen
+                setNeedsDisplay()
+            }
+        } else {
+            // If no line is selected, do not do anything
+            return
+        }
+    }
+    
+    override var canBecomeFirstResponder: Bool{
+        return true
+    }
+    
+    @objc func tap(_ gestureRecognizer: UIGestureRecognizer){
+        let point = gestureRecognizer.location(in: self)
+        selectedIndex = indexOfLine(at: point)
+        
+        let menu = UIMenuController.shared
+        if selectedIndex != nil{
+            becomeFirstResponder()
+            let deleteItem = UIMenuItem(title: "Delete", action: #selector(DrawView.deleteLine(_:)))
+            menu.menuItems = [deleteItem]
+            let targetRect = CGRect(x: point.x, y: point.y, width: 2, height: 2)
+            menu.setTargetRect(targetRect, in: self)
+            menu.setMenuVisible(true, animated: true)
+        }else{
+            menu.setMenuVisible(false, animated: true)
+        }
+        
+        
+        setNeedsDisplay()
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
     
     func stroke(_ line: Line){
@@ -49,6 +161,31 @@ class DrawView: UIView{
         for (_, line) in currentLine {
             stroke(line)
         }
+        
+        if let index = selectedIndex{
+            UIColor.purple.setStroke()
+            let selectedLine = finishedLines[index]
+            stroke(selectedLine)
+        }
+    }
+    
+    func indexOfLine(at point: CGPoint) -> Int? {
+        // Find a line close to point
+        for (index, line) in finishedLines.enumerated() {
+            let begin = line.begin
+            let end = line.end
+            // Check a few points on the line
+            for t in stride(from: CGFloat(0), to: 1.0, by: 0.05) {
+                let x = begin.x + ((end.x - begin.x) * t)
+                let y = begin.y + ((end.y - begin.y) * t)
+                // If the tapped point is within 20 points, let's return this line
+                if hypot(x - point.x, y - point.y) < 20.0 {
+                    return index
+                }
+            }
+        }
+        // If nothing is close enough to the tapped point, then we did not select a line
+        return nil
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
